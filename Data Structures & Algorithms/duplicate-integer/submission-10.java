a707824b2d48f39d391cc727d978ea553d5c60f5
poornimasrikantesh <poornima.srikantesh@gmail.com>
2026-08-03 21:06:11 -0700

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet hs = new HashSet<>();
+        for(int n : nums){
+            if(hs.contains(n)){
+                return true;
+            }
+            hs.add(n);
+        }
+        return false;
+    }
+}
